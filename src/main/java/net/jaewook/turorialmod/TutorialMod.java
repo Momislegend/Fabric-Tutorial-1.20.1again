9d1feff985c7e3259c71fa5d9e39515c77b0b5e9
jwkjw <jwkjwk050312@gmail.com>
2025-05-11 08:18:25 +0900

    Initial commit

---
+++ src/main/java/net/jaewook/turorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.jaewook.turorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jaewook/turorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.jaewook.turorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
